8c445510444baa55db7fc397abd0654ebacd4ef9
João Rafael Sanches <jsanchesedantas@gmail.com>
2025-04-02 20:38:08 -0300

    att ex 3

--- src/exemplos/ex-03/index.js
+++ src/exemplos/ex-03/index.js
@@ -1,24 +1,43 @@
-import { View, Text } from "react-native";  // Importando os componentes View e Text do pacote react-native
+import { View, Text, Button, Alert, TouchableOpacity } from 'react-native';
 
-import styles from "./styles"; // Importando o objeto styles do arquivo styles.js
+import styles from './styles';
 
+function Exemplo3() {  
 
-function Exemplo3 () {
-  return (
-    <View style={styles.container}>
+    function handleExibeMensagem () {
+        Alert.alert('Teste')
+    }
 
-      <Text style={styles.titulo}>Exemplo3</Text>
-      <Text style={styles.texto}>aula de introdução ao expo</Text>
-      <Button
-         onPress={() => {}}
-         title="Alerta"
-         color="deepskyblue"
-         accessibilityLabel="mensagem de alerta"
-         />
+    const handleOutroJeitoFuncao = () => {
+        Alert.alert('Título', 'Texto mensagem', [
+            {
+              text: 'Meu texto',
+              onPress: () => Alert.alert('Tchau'),
+              style: 'default',
+            },
+          ])
+    }
 
-    </View>
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Exemplo 3</Text>
+            <Text style={styles.texto}>Variáveis vs State</Text>
+            <View style={styles.ButtonAlert}>
+              <Button
+                onPress={handleOutroJeitoFuncao}
+                title="Alerta"
+                color="deepskyblue"
+                accessibilityLabel="Mensagem de alerta"                  
+               />
+            </View>
+            <Text>VALOR</Text>
+            <TouchableOpacity>
+                <Text style={styles.TouchableOpacity}>Adicioar mais 1</Text>
+            </TouchableOpacity>
 
-  );
+        </View>
+    );
 }
 
-export default Exemplo3; // Exportando o componente Exemplo1
+export default Exemplo3;
+
--- src/exemplos/ex-03/styles.js
+++ src/exemplos/ex-03/styles.js
@@ -1,30 +1,45 @@
-import { StyleSheet } from "react-native"; // Importando o pacote StyleSheet do react-native
-import { RFPercentage } from "react-native-responsive-fontsize";
+import { StyleSheet, TouchableOpacity } from 'react-native'; 
+import { RFPercentage } from 'react-native-responsive-fontsize';
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    padding: 8,
-    flex: 1,
-    alignItems: 'center',
-  },
-  titulo: {
-    fontSize: RFPercentage(3),
-    fontWeight: 'bold',
-    borderWidth: 2,
-    borderColor: 'deepskyblue',
-    marginTop: 10,
-    marginBottom:10,
-    padding: 8,
-    width: '100%',
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    height: 80,
-  },
-  texto: {
-    fontSize: RFPercentage(2.2),
-    color: 'deepskyblue',
-  },
+    container: {
+        backgroundColor: '#fff', 
+        padding: 8, 
+        flex: 1, 
+        alignItems: 'center', 
+        borderRadius: 20,
+    }, 
+    titulo: {
+        fontSize: RFPercentage(3), 
+        color: 'red', 
+        fontWeight: 'bold', 
+        borderWidth: 2, 
+        borderColor: 'red', 
+        marginTop: 10, 
+        marginBottom: 10, 
+        padding: 8, 
+        width: '100%', 
+        height: 80, 
+        textAlign: 'center', 
+        textAlignVertical: 'center', 
+        borderRadius: 20,
+    }, 
+    texto: {
+        fontSize: RFPercentage(2.2),  
+    }, 
+    ButtonAlert: {
+        margin: RFPercentage(1.5),
+        width:'100%',
+        //height:'100%',
+    },
+    TouchableOpacity: {
+        padding: RFPercentage(1), 
+        alignItems: 'center', 
+        borderRadius: 20,
+        backgroundColor:'red',
+        justifyContent:'center',
+    }
 });
 
-export default styles; // Exportando o objeto styles
+export default styles;
+
